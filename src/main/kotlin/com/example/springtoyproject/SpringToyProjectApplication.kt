1e4ee7cc69e28f3c878141f94a4fbe13645243ad
Mac_yj <pknb213@naver.com>
2022-04-09 22:31:52 +0900

    Added basic

---
+++ src/main/kotlin/com/example/springtoyproject/SpringToyProjectApplication.kt
@@ -0,0 +1,12 @@
+package com.example.springtoyproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringToyProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringToyProjectApplication>(*args)
+    println("@#$#@#@$ Main #$@$#$@#")
+}
---
+++ src/test/kotlin/com/example/springtoyproject/SpringToyProjectApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.example.springtoyproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringToyProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+        println("@!@!@!@ Test @!@!")
+    }
+
+}
