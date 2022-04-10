3818669887e89e7a12eb4df9f6d856f802c10145
Mac_yj <pknb213@naver.com>
2022-04-10 23:15:32 +0900

    Updated

--- src/main/kotlin/com/example/springtoyproject/SpringToyProjectApplication.kt
+++ src/main/kotlin/com/example/springtoyproject/SpringToyProjectApplication.kt
@@ -2,6 +2,8 @@ package com.example.springtoyproject
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class SpringToyProjectApplication
@@ -10,3 +12,14 @@ fun main(args: Array<String>) {
     runApplication<SpringToyProjectApplication>(*args)
     println("@#$#@#@$ Main #$@$#$@#")
 }
+@RestController
+class MessageResource {
+    @GetMapping
+    fun index(): List<Message> = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
+}
+
+data class Message(val id: String?, val text: String)
