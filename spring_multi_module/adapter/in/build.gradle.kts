2690d23e99c769b04fa3340f95655939f8f6f0a6
youngjo.cheon <youngjo.cheon@dong-a.com>
2024-03-04 09:27:18 +0900

    MOD

---
+++ spring_multi_module/adapter/in/build.gradle.kts
@@ -0,0 +1,19 @@
+description = "adapter in module"
+
+plugins {
+//    id("kotlin-spring-conventions")
+    // Documentation
+//    id("org.asciidoctor.jvm.convert") version "3.1.0"
+}
+
+dependencies {
+    // Spring 의존성
+//    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Documentation
+//    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+
+    // test fixtures
+//    testImplementation(testFixtures(project(":domain")))
+}
+
